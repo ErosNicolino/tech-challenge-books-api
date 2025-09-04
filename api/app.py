feae9f828390765d8e967277e328fa1162434f79
ErosNicolino <erosnicolino@icloud.com>
2025-09-04 10:04:29 -0300

    Fix Heroku boot timeout

--- api/app.py
+++ api/app.py
@@ -39,5 +39,6 @@ def list_categories():
     categories = sorted(list(set(b['category'] for b in books)))
     return jsonify(categories)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
